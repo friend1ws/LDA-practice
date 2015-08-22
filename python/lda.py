cae4c2c038c24c0ec058c1ae8fe95389453b4b8c
friend1ws <friend1ws@gmail.com>
2015-08-23 08:23:41 +0900

    LDA python 85% completion

--- python/lda.py
+++ python/lda.py
@@ -14,13 +14,13 @@ class LDA:
         self.beta = 0.1
     
         # initialize
-        self.word_count = [[0] * self.topic_num] * self.word_num
-        self.doc_count = [[0] * self.topic_num] * self.doc_num
-        self.topic_count = [0] * self.topic_num
-        self.z = [0] * len(self.token_list)
+        self.word_count = [[0 for k in range(self.topic_num)] for w in range (self.word_num)]
+        self.doc_count = [[0 for k in range(self.topic_num)] for d in range(self.doc_num)]
+        self.topic_count = [0 for k in range(self.topic_num)]
+        self.z = [0 for i in range(len(self.token_list))]
 
         # add token information
-        for i in rannge(len(self.token_list)):
+        for i in range(len(self.token_list)):
             assign = random.randint(0, self.topic_num - 1)
             self.doc_count[token_list[i][0]][assign] += 1
             self.word_count[token_list[i][1]][assign] += 1
@@ -30,7 +30,7 @@ class LDA:
 
     def update(self):
         for i in range(len(self.token_list)):
-            resample(i)
+            self.resample(i)
 
 
     def resample(self, i):
@@ -50,13 +50,28 @@ class LDA:
 
         u = random.random() * p[self.topic_num - 1]
         for k in range(self.topic_num):
-            if u < p[k]: assign = k
-
+            if u < p[k]: 
+                assign = k
+                break
 
         self.doc_count[self.token_list[i][0]][assign] += 1
         self.word_count[self.token_list[i][1]][assign] += 1
         self.topic_count[assign] += 1
         self.z[i] = assign
 
-    
-    
+
+    def get_phi(self):
+        phi = [[0.0 for d in range(self.word_num)] for k in range(self.topic_num)]
+        for k in range(self.topic_num):
+            sum = 0.0
+            for j in range(self.word_num):
+                phi[k][j] = self.beta + float(self.word_count[j][k])
+                sum += phi[k][j]
+
+            sinv = 1.0 / float(sum)
+            for j in range(self.word_num):
+                phi[k][j] *= sinv
+        
+        return phi
+
+
